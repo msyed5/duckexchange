3fbe76ed49510202e898db1b2a1435a98bb5039e
msyed5 <77374947+msyed5@users.noreply.github.com>
2022-04-25 14:27:10 -0400

    Add filter and search bar

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { authentication, db } from "../backend/firebase-config";
 
+
 function Browse({ signedIn }) {
-  const [postLists, setPostList] = useState([]);
-  const postsCollectionRef = collection(db, "posts");
+  const [items, setItems] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  const [searchKey, setSearchKey] = useState("");
+  const [filterType, setFilterType] = useState("");
+  const users = collection(db, "posts");
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  // async function getData() {
+  //   try {
+  //     const users = await getDocs(collection(db, "posts"));
+  //     const productsArray = [];
+  //     users.forEach((doc) => {
+  //       const obj = {
+  //         id: doc.id,
+  //         ...doc.data(),
+  //       };
+  //     });
+  //
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+
+
+
 
   const deletePost = async (id) => {
     const postDoc = doc(db, "posts", id);
@@ -16,8 +47,8 @@ function Browse({ signedIn }) {
 
   useEffect(() => {
     const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      const data = await getDocs(users);
+      setItems(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getPosts();
@@ -25,19 +56,47 @@ function Browse({ signedIn }) {
 
   return (
     <div className="homePage">
-      {postLists.map((post) => {
+    <input
+            type="text"
+            value={searchKey}
+            onChange={(e) => {
+              setSearchKey(e.target.value);
+            }}
+            className="form-control mx-2"
+            placeholder="search items"
+
+          />
+          <select
+                  className="form-control mt-3"
+                  value={filterType}
+                  onChange={(e) => {
+                    setFilterType(e.target.value);
+                  }}
+                >
+                  <option value="">All</option>
+                  <option value="Books">Books</option>
+                  <option value="Clothing">Clothing</option>
+                  <option value="Furniture">Furniture</option>
+                  <option value="Electronics">Electronics</option>
+                  <option value="Sports Gear">Sports Gear</option>
+                </select>
+      {items
+
+            .filter((data) => data.title.toLowerCase().includes(searchKey))
+            .filter((data) => data.category.includes(filterType))
+            .map((item) => {
         return (
-          <div className="post">
+          <div className="item">
             <div className="postHeader">
               <div className="title">
-                <h1> {post.title}</h1>
+                <h1> {item.title}</h1>
               </div>
               <div className="deletePost">
-                {signedIn && post.author.id === authentication.currentUser.uid
+                {signedIn && item.author.id === authentication.currentUser.uid
                 //   && (
                 //   <button
                 //     onClick={() => {
-                //       deletePost(post.id);
+                //       deletePost(item.id);
                 //     }}
                 //   >
                 //     {" "}
@@ -47,11 +106,14 @@ function Browse({ signedIn }) {
               }
               </div>
             </div>
-            <div className="postTextContainer"> Category: {post.category} </div>
-            <div className="postTextContainer"> Price: {post.price} </div>
-            <div className="postTextContainer"> Condition: {post.condition} </div>
-            <div className="postTextContainer"> Description: {post.postText} </div>
-            <p>Sold By: {post.author.name}</p>
+            <div className="postTextContainer"> Category: {item.category} </div>
+            <div className="postTextContainer"> Price: {item.price} </div>
+            <div className="postTextContainer"> Condition: {item.condition} </div>
+            <div className="postTextContainer"> Description: {item.postText} </div>
+            <p>Sold By: {item.author.name}</p>
+            <button className="btn btn-info" > View Item </button>
+            <button className="btn btn-info" > Post Item </button>
+
           </div>
         );
       })}
