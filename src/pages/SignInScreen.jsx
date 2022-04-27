8a0db0aa1b7e32ee8614bc13e2961d616b1ab4e9
mxfu <mphu@stevens.edu>
2022-04-26 23:51:36 -0400

    new changes

--- src/pages/SignInScreen.jsx
+++ src/pages/SignInScreen.jsx
@@ -1,14 +1,9 @@
 import { authentication } from '../backend/firebase-config';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-/* import logo from "./logo.png";
-console.log(logo); */
-=======
-import logo from '/Users/serenalee/duckexchange/src/logoduck.png';
+import logo from 'C:\ssw345project\duckexchange4\duckexchange\src\logoduck.png';
 
 console.log(logo);
->>>>>>> 66eadfe214649e251efcca84e391f8580786ce9a
 
 
 const SignInScreen = ({ setSignedIn }) => {
